5aa60094d394a35ff779b5842d8988c39836a2ba
Sadique Barlaskar <129667908+Sk4373@users.noreply.github.com>
2025-05-21 22:28:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -78,6 +78,22 @@ async def is_subscribed(bot, user_id, channel_id):
             return True
     return False
     
+#-------------------------------------------------------------------
+async def infinitymp_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append(
+                [InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)]
+            )
+        except Exception as e:
+            pass
+    return btn
+#-------------------------------------------------------------------
+
 async def is_check_admin(bot, chat_id, user_id):
     try:
         member = await bot.get_chat_member(chat_id, user_id)
